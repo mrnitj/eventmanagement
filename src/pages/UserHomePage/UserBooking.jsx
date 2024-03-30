6a535f7dc479d85a40eefcc307c2867e0f6e8db3
Your Name <razzrahul789@gmail.com>
2024-03-30 15:44:30 +0530

    orders page

--- src/pages/UserHomePage/UserBooking.jsx
+++ src/pages/UserHomePage/UserBooking.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import QRCode from "qrcode.react";
+import axios from "../../utils/AxiosInstance"
 
 export const UserBooking = () => {
 
-  
+  const [bookingData, setBookingData] = useState([]);
+
+  console.log("bookingdata",bookingData)
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/api/fetchallbooking");
+      setBookingData(response.data?.data);
+
+   
+    } catch (err) {
+      console.error("event fetching error:", err);
+      console.log("Response:", err.response);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
     <>
       <QRCode
